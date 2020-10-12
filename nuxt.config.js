05d58a7288f2fdb3ab6fe2127539201869a826a0
kn0wn <admin@joshking.dev>
2020-10-12 17:35:02 +0100

    chore: added a favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'COVID-19 - UK',
+    base: { target: '_blank' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
